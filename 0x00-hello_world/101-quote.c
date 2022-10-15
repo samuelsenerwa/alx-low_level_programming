dff2e96aa657e74fa1aa58e4bede8af40696e1b8
samuelsenerwa <samuelsenerwa@gmail.com>
2022-10-15 13:37:47 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -14,10 +14,10 @@ int main(void)
 	long syscall = 1;
 	long ret = 0;
 	__asm__ ("syscall"
-			: "=a" (ret)
-			: "a" (syscall),
-			"D" (fd),
-			"S" (s),
-			"d" (l));
+	: "=a" (ret)
+	: "a" (syscall),
+	"D" (fd),
+	"S" (s),
+	"d" (l));
 	return (1);
 }
