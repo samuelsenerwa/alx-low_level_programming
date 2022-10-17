d170150b0b3db8f1cfd6db5eb238324e29c431ef
samuelsenerwa <samuelsenerwa@gmail.com>
2022-10-17 00:40:20 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	int i;
 
-	for (i ='0'; i <= '9'; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
 		putchar (i);
 
-		if(i != '9')
+		if (i != '9')
 		{
 			putchar(',');
 			putchar(' ');
