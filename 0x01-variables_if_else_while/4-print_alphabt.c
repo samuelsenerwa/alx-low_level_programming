e7027948fdba09d72505868d19059df75a494296
samuelsenerwa <samuelsenerwa@gmail.com>
2022-10-16 23:36:11 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int ch;
 
-	for (ch = 'a'; ch <='z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		if(ch != 'e' && ch != 'q')
+		if (ch != 'e' && ch != 'q')
 		{
 			putchar(ch);
 		}
