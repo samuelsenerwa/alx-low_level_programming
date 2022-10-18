990b805af9146b3947175612826f7045955b1960
samuelsenerwa <samuelsenerwa@gmail.com>
2022-10-18 15:02:15 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,16 @@
  */
 int main(void)
 {
-	char str[] = "Holberton\n";
-	int i = 0;
-	
-	while (str[i] != '\0')
-		{
-			char c = str[i];
+char str[] = "Holberton\n";
+int i = 0;
 
-			_putchar(c);
-			i++;
-		}
-		return (0);
+while (str[i] != '\0')
+	{
+	char c = str[i];
+
+	_putchar(c);
+	i++;
+	}
+return (0);
 }
+
