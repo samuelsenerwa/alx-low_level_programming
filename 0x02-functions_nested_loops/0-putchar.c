62224703d941a66d47450da5985a320c98b04911
samuelsenerwa <samuelsenerwa@gmail.com>
2022-10-18 14:53:27 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	char str[] = "Holberton\n";
 	int i = 0;
-
+	
 	while (str[i] != '\0')
 		{
 			char c = str[i];
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 /**
  * print_alphabet - Entry point
  * Description: a function that prints the alphabet,
