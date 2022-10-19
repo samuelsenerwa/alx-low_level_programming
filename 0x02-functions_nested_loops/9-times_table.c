7bd5634da998f1450b04ac203b265e259c7b33db
samuelsenerwa <samuelsenerwa@gmail.com>
2022-10-19 14:41:29 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -26,12 +26,13 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(ones + '0');
+				_putchar(' ');
+				_putchar( ones + '0');
 			}
 			else
-		       	{
-				_putchar(',');
-				_putchar(' ');
+			{
+				_putchar(', ');
+				_putchar(' );
 				_putchar(tens + '0');
 				_putchar(ones + '0');
 			}
