90de7facde03b4e6304210707e473758b15f9497
samuelsenerwa <samuelsenerwa@gmail.com>
2022-10-19 16:08:37 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,12 +16,20 @@ void print_times_table(int n)
 		{
 			for (y = 0; y <= n; y++)
 			{
-			       	z = x * y;
+				z= x * y;
 				if (z > 99)
 				{
 					_putchar(',');
 					_putchar(32);
 					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else if (z > 9)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
 					_putchar(((z / 10) % 10) + '0');
 					_putchar((z % 10) + '0');
 				}
