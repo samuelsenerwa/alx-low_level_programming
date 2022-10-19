683d202e2637596c4aa4ad0b1eff88d93a2ff5df
samuelsenerwa <samuelsenerwa@gmail.com>
2022-10-19 08:50:07 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -25,7 +25,6 @@ void  jack_bauer(void)
 	while (hours_ones < hours_max)
 	{
 		minutes_tens = '0';
-		
 		while (minutes_tens < '6')
 		{
 			minutes_ones = '0';
@@ -46,6 +45,6 @@ void  jack_bauer(void)
 		minutes_tens = '0';
 		hours_ones++;
 	}
-	hours_ones ='0';
+	hours_ones = '0';
 	hours_tens++;
 }
