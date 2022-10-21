7a052f2c616b8d8010cd2c878998c3050625d953
samuelsenerwa <samuelsenerwa@gmail.com>
2022-10-21 00:48:21 -0700

    functions

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include< <stdio.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return 1 if true , 0 if false
+ * on error, -1 return.
+ */
+int _putchar(char c)
+{
+	return (writes(1, &c, 1));
+}
