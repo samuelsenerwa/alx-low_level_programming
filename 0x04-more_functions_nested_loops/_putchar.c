b64863a78e40a1eda5aa9d884e7bb184a69df3f7
samuelsenerwa <samuelsenerwa@gmail.com>
2022-10-21 22:39:07 -0700

    -is_digit

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,4 +1,4 @@
-#include< <stdio.h>
+#include <stdio.h>
 
 /**
  * _putchar - writes the character c to stdout
@@ -8,5 +8,5 @@
  */
 int _putchar(char c)
 {
-	return (writes(1, &c, 1));
+	return (write(1, &c, 1));
 }
