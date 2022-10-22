094706140396a610197215a4572ad1638d21097a
samuelsenerwa <samuelsenerwa@gmail.com>
2022-10-22 01:13:49 -0700

    putchar

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,17 +1,15 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _isdigit.c - checks if a character is digit
- * @c: carrier variable
+ * _isdigit - checks if the character is a digit
+ * @c: it is the variable
  * Return: 1 if true, 0 if false
- *
- */
-
+*/
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
 		return (1);
-
 	else
 		return (0);
 }
