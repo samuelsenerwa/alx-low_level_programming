f7d77f063ec72ae2adb7d0d2596c86d84680280e
samuelsenerwa <samuelsenerwa@gmail.com>
2022-11-03 15:25:02 -0700

    Added task 5 on project 0x08

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -18,7 +18,7 @@ int _sqrt_recursion(int n)
 /**
  * actual_sqrt_recursion - recurse to find the natural
  * square root of a number
- * @n: number to calculate the sqaure root of 
+ * @n: number to calculate the sqaure root of
  * @i: iterator
  *
  * Return: the resulting square root
