b0ad0a7544496c03df65df1fb6d146b895eb2c93
samuelsenerwa <samuelsenerwa@gmail.com>
2022-11-03 14:41:10 -0700

    Added task 1 to 0x08

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the length of
+ *
+ * Return:length of the string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
