03566f461203769fa8df93f57642c23a967dc920
samuelsenerwa <samuelsenerwa@gmail.com>
2022-11-09 14:40:19 -0800

    task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 /**
  *argstostr - concatenates all arguments of the program
- *@x: argument count
+ *@ac: argument count
  *@av: pointer to array of size ac
  *Return: NULL if ac ==0 or av == null, Pointer to new string
  *NULL on fail
