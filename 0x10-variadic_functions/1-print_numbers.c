98359829f116bc312f73f92335c77d1d731ad299
samuelsenerwa <samuelsenerwa@gmail.com>
2022-11-18 16:41:16 -0800

    task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,23 @@
+#include "vardiac_functions.h"
+
+/**
+ * sum_them_all - calculates the sum of all its parameter
+ * @n: number of arguments passed to the function
+ *
+ * Return: the resulting sum
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+	va_list;
+
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(list, int);
+
+	va_end(list);
+
+	return (sum);
+}
