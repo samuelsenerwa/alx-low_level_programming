b94b5d3a597f932b439ba6b5fc05bff588983947
samuelsenerwa <samuelsenerwa@gmail.com>
2022-11-19 18:03:50 -0800

    task 4

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -19,8 +19,8 @@ typedef struct op
 } op_t;
 
 int op_add(int a, int b);
-int op_mul(int a, int b);
 int op_sub(int a, int b);
+int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
