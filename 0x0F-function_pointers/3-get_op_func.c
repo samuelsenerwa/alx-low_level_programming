ab13b1c30d07d26083785a3aa2914b094fa45c8c
samuelsenerwa <samuelsenerwa@gmail.com>
2022-11-19 18:28:26 -0800

    task 4

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -23,11 +23,15 @@ int (*get_op_func(char *s))(int, int)
 
 	i = 0;
 
-	while (ops[i].op)
+	while (i < 5)
 	{
-		if (strcmp(ops[i].op, s) == 0)
+		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
+
 		i++;
+
 	}
-	return (NULL);
+
+	return (0);
+
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,9 +19,6 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	arg1 = atoi(argv[1]);
-	arg2 = atoi(argv[3]);
-
 	func = get_op_func(argv[2]);
 
 	if (!func)
@@ -30,15 +27,6 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	o = *argv[2];
-
-	if ((o == '/' || o == '%') && arg2 == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	result = func(arg1, arg2);
-
+	printf("%d\n", func(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
