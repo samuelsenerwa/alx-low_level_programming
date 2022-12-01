a22182bb5073cd64cb229e690b50de6a049ed4fd
samuelsenerwa <samuelsenerwa@gmail.com>
2022-11-30 18:39:10 -0800

    task added

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -6,6 +6,7 @@
  *   @idx: index of the node to be added
  *  @n: content of the new node
  *  
+ *  Description: functon to insert into link list
  *     Return: the address of the node
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
