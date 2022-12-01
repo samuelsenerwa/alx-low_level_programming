bd6b696c4b77243687c9c4c22692ddbb6ef46d49
samuelsenerwa <samuelsenerwa@gmail.com>
2022-11-30 18:52:27 -0800

    task added

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -5,6 +5,7 @@
  *  @head: pointer to the first element in the list
  *  @index: index of the node to delete
  *  
+ *  Descritpion: functions that delete node int at  given index  
  *   Return: 1 (Success), or -1 (Fail)
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
