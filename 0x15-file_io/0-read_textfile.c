97cc0c94bee8b6d6653623df1d062b8ce0fddcec
samuelsenerwa <samuelsenerwa@gmail.com>
2022-12-08 17:53:13 -0800

    task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,7 +17,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	buff = malloc(sizeof(char) *letters);
+	buff = malloc(sizeof(char) * letters);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
@@ -26,6 +26,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	check = read(fd, buff, letters);
 	if (check == -1)
 		return (0);
+
 
 	len = write(STDOUT_FILENO, buff, check);
 	close(fd);
