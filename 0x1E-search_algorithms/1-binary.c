08e721346bf00fb3cf43fc9ba3102f42833cba6a
samuelsenerwa <samuelsenerwa@gmail.com>
2023-05-09 07:20:06 -0700

    algorithm

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -23,7 +23,7 @@ int binary_search(int *array, size_t size, int value)
 	{
 		printf("Searching in array :");
 		for (i = left; i < right; i++)
-			printf("%d". array[i]);
+			printf("%d", array[i]);
 		printf("%d\n", array[i]);
 
 		i = left + (right - left) / 2;
