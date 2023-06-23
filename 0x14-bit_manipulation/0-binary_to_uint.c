3ff572fe66a38338b69d567d0ff3f16c27331008
Samuel Senerwa <samuelsenerwa@gmail.com>
2023-06-23 05:07:19 -0700

    checker correction

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,12 +1,12 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * binary_to_unit - converts a binary number to an unsigned int
- * @b: char string
- * Return: converted number or 0 if there is an unconvertable char
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int.
  */
-
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int ui;
@@ -20,7 +20,7 @@ unsigned int binary_to_uint(const char *b)
 	for (len = 0; b[len] != '\0'; len++)
 		;
 
-	for (len--; base_two = 1; len >= 0; len--, base_two *=2)
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
 		if (b[len] != '0' && b[len] != '1')
 		{
