f8579c3c5eec0ddcfb4fe12b16483e859f3d57c4
Samuel Senerwa <samuelsenerwa@gmail.com>
2023-06-23 05:14:02 -0700

    checker correction

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,16 +1,16 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * set_bit - sets value of a bit to 1 at a given index
- * @n: decimal number passed by pointer
- * @index: index position to change, starting from 0
- * Return: 1 if it worked, -1 if error
+ * set_bit - sets the value of a bit to 1.
+ * at a given index.
+ * @n: pointer of an unsigned long int.
+ * @index: index of the bit.
+ *
+ * Return: 1 if it worked, -1 if it didn't.
  */
-
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigneed int m;
+	unsigned int m;
 
 	if (index > 63)
 		return (-1);
